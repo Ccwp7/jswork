9764bc98e1e9b43a8ca950d1aa0779a99bcbfb02
ccwp7 <2398083734@qq.com>
2019-12-04 21:29:23 +0800

    work12-1

---
+++ work12-1/index1.js
@@ -0,0 +1,11 @@
+//ar w = new Date.getDay();
+function calendar(y) {
+//let year =  new Date().getFullYear()
+document.getElementById("cal").innerHTML = calendar(year)
+
+function setCalendar(year){
+ preyear
+ curyear
+ nextyear
+    car
+}}
---
+++ work12/index.js
@@ -0,0 +1,6 @@
+let year =  new Date().getFullYear()
+document.getElementById("cal").innerHTML = calendar(year)
+
+function setCalendar(year){
+    
+}
